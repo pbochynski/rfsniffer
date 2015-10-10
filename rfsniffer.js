30b6ee93cc847b24d88785f1f3034b7a1b1213ca
Bochynski, Piotr <piotr.bochynski@hybris.de>
2015-10-10 21:33:04 +0200

    Added time and only switch logging.

--- rfsniffer.js
+++ rfsniffer.js
@@ -1,8 +1,16 @@
 var gpio = require('rpi-gpio');
 
+var state = null;
+var start = process.hrtime();
+
 gpio.on('change', function(channel, value) {
-	console.log(channel + ' = ' + value);
+	if (state!=value) {
+		var diff = process.hrtime(start);
+		console.log(channel + ' = ' + value+', time: %s s, %s ns',diff[0], diff[1]);
+		state = value;
+	}
 });
+
 gpio.setup(13, gpio.DIR_IN, gpio.EDGE_BOTH, function(err){
 	if (err) {
 		return console.error("Cannot set pin 13: %s", err);
