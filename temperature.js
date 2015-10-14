ec4180f038328eb8e4e2e02cc2ae331c71fdbe8a
Bochynski, Piotr <piotr.bochynski@hybris.de>
2015-10-14 23:23:53 +0200

    read temperature

--- temperature.js
+++ temperature.js
@@ -5,7 +5,10 @@ ds18b20.sensors(function(err, ids) {
 });
 
 // ...
+function readTemp() {
+	ds18b20.temperature('28-031467bacdff', function (err, value) {
+		console.log('Current temperature is', value);
+	});
+}
 
-ds18b20.temperature('28-031467bacdff', function(err, value) {
-	console.log('Current temperature is', value);
-});
+setInterval(5000,readTemp);
