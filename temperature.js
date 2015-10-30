db057334ffcccbbdf54fb921c74ecb7cd771edbe
Bochynski, Piotr <piotr.bochynski@hybris.de>
2015-10-30 19:01:06 +0100

    Simple regulator

--- temperature.js
+++ temperature.js
@@ -1,20 +1,42 @@
 var ds18b20 = require('ds18b20');
-
+var RADIATOR = '28-031467bacdff';
+var ROOM = '28-0115621a55ff';
+var heat = require('./heat');
 
 ds18b20.sensors(function(err, ids) {
 	ids.forEach(function (id){
 			console.log(id);
 			setInterval(function(){
 				readTemp(id);
-			}, 5000);
+			}, 10000);
 		}
 	);
 });
 
+function getDate() {
+  return new Date().toISOString().
+	  replace(/T/, ' ').      // replace T with a space
+	  replace(/\..+/, '');     // delete the dot and everything after
+}
+
 function readTemp(id) {
 	ds18b20.temperature(id, function (err, value) {
-		console.log(id+': ', value);
-
+		console.log(getDate()+' '+id+': ', value);
 	});
 }
 
+function regulator() {
+	ds18b20.temperature(ROOM, function (err, value) {
+		if (err) return;
+		if (value > 21.7) {
+			heat.off();
+			console.log(getDate()+': Heat Off');
+		}
+		if (value < 21.5) {
+			heat.on();
+			console.log(getDate()+': Heat On');
+		}
+	});
+}
+
+setInterval(regulator, 60000);
