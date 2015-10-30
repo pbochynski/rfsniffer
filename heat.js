9f47dcb1ddeacf45a5974b8cb0606ef14f96c563
Bochynski, Piotr <piotr.bochynski@hybris.de>
2015-10-30 19:08:56 +0100

    clean logs

--- heat.js
+++ heat.js
@@ -13,8 +13,6 @@ function heatOff() {
 
 function codesend(code) {
 	exec(CODESEND + code, function (error, stdout, stderr) {
-		console.log('stdout: ' + stdout);
-		console.log('stderr: ' + stderr);
 		if (error !== null) {
 			console.error('exec error: ' + error);
 		}
